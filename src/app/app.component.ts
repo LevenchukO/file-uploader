bfcd6b6caccc2f6e0d39001c7a1b218da4ee97b3
peleador <levenchuk.o@gmail.com>
2018-08-27 00:05:35 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FileQueueObject, FileUploaderService } from './file-uploader.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'file-uploader';
+export class AppComponent  implements OnInit {
+
+  @Output() onCompleteItem = new EventEmitter();
+
+  @ViewChild('fileInput') fileInput;
+  queue: Observable<FileQueueObject[]>;
+
+  constructor(public uploader: FileUploaderService) { }
+
+  ngOnInit() {
+    this.queue = this.uploader.queue;
+    this.uploader.onCompleteItem = this.completeItem;
+  }
+
+  completeItem = (item: FileQueueObject, response: any) => {
+    this.onCompleteItem.emit({ item, response });
+  }
+
+  addToQueue() {
+    const fileBrowser = this.fileInput.nativeElement;
+    this.uploader.addToQueue(fileBrowser.files);
+  }
 }
